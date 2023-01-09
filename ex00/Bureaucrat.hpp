a4b3d50e7c4c58ec3509fc1ffd4b5131e41b00fc
jhj9109 <janghj18@gmail.com>
2023-01-10 00:49:20 +0900

    Add ex00 files

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+    static const unsigned int MAX_GRADE = 1;
+    static const unsigned int MIN_GRADE = 150;
+
+    // 아래를 private라고 명시하지는 않음
+    const std::string name_; // const로 명시
+    unsigned int grade_;
+
+public:
+    Bureaucrat(const std::string &name = "anonymous", const unsigned int grade = 150);
+    Bureaucrat(const Bureaucrat &bureaucrat);
+    Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+    virtual ~Bureaucrat(void);
+
+    // getter
+    const std::string &getName(void) const;
+    unsigned int getGrade(void) const;
+
+    // setter 랄까?
+    void increaseGrade(void);
+    void decreaseGrade(void);
+
+    // 오류
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        virtual const char *what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        virtual const char *what() const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
